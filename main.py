d8cbece346072e830da61e4275511b05d9d7af00
SeanYang32 <seanyang1@gmail.com>
2023-10-24 17:38:44 -0400

    Encoder and Menu

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+def encode(originalString):
+    finalString = ""
+    for i in range(0, len(originalString)):
+        tempInt = (int(originalString[i:i+1]) + 3) % 10
+        finalString = finalString + str(tempInt)
+    return finalString
+
+
+def encodeDigit(num):
+    return str((num + 3) % 10)
+
 def main():
-    print("This is a test")
-    print("This is test 2")
+    stillRunning = True
+    encodedPassword = ""
+    while stillRunning:
+        selection = input("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n\nPlease enter an option: ")
+        if selection == "1":
+            passwordInput = input("Please enter your password to encode: ")
+            encodedPassword = encode(passwordInput)
+            print("Your password has been encoded and stored!")
+        if selection == "3":
+            stillRunning = False
+
+
 
 if __name__ == '__main__':
     main()
